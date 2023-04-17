744bbf531cf718119e9e0c708872b5281d1e1f96
Łukasz Łukasiewicz <lukasz.luk@o2.pl>
2023-04-17 22:25:04 +0200

    Returning HTML, views & dynamic view logic.

--- Section3/monthly_challenges/challenges/views.py
+++ Section3/monthly_challenges/challenges/views.py
@@ -21,6 +21,21 @@ monthly_challenges = {
 }
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"f\">{capitalized_month}</a></li>"
+
+    response_data = f"""
+        <ul>{list_items}</ul>
+    """
+
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
@@ -33,6 +48,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This mount is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
